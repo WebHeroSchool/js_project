2d5773a4b468d6822d9b61fe4a86bd1eb2f26376
j-sinetskaya <egd1989@mail.ru>
2021-02-25 14:47:07 +0300

    add variables num and score and objects for questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
-let hello;
-hello='Привет';
-alert(hello);
+const question1 = {
+    text: 'Вопрос 1'
+  },
+  question2 {
+    text: 'Вопрос 2'
+  },
+  question3 {
+    text: 'Вопрос 3'
+  },
+  question4 {
+    text: 'Вопрос 4'
+  };
+
+let num,
+  score;
