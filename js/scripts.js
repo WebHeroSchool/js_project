1b3ac28449dfd242bf0f6963a6c2730d2154a252
j-sinetskaya <egd1989@mail.ru>
2021-03-03 16:20:44 +0300

    add function answers check

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,34 @@
 let num,
   score;
 
-// 3. В проекте - преобразовать объекты с заданиями и ответами в единный массив = [question 1, question 2, question 3, question 4]
 const questionsArray = [
   {
     question: 'Вопрос 1',
-    answer: 'Ответ 1'
+    correctAnswer: 'Ответ 1'
   },
   {
     question: 'Вопрос 2',
-    answer: 'Ответ 2'
+    correctAnswer: 'Ответ 2'
   },
   {
     question: 'Вопрос 3',
-    answer: 'Ответ 3'
+    correctAnswer: 'Ответ 3'
   },
   {
     question: 'Вопрос 4',
-    answer: 'Ответ 4'
-  }
-];
-// 1. Сделать массив [question 1, question 2], вывести в консоль correctAnswer для кажого элемента массива
-let questArray = [
-  {
-    question1: 'Вопрос 1',
-    correctAnswer: 'a'
-  },
-  {
-    question2: 'Вопрос 2',
-    correctAnswer: 'b'
+    correctAnswer: 'Ответ 4'
   }
 ];
 
-questArray.forEach((item) => {
-  console.log(item.correctAnswer);
-});
+function quiz(array) {
+  array.forEach((item) => {
+    let answer = prompt(item.question, '').toLowerCase();
+    if (answer === item.correctAnswer.toLowerCase()) {
+      alert('Верно!')
+    } else {
+      alert('Неверно!')
+    }
+  })
+}
 
-// 2. Из массива в задании 1, сделать массив [question3,question 1, question 2], вывести correctAnswer только там, где он равен "c"
-questArray.unshift ({question3: 'Вопрос 3', correctAnswer: 'c'})
-console.log(questArray);
-
-questArray.forEach((item) => {
-  if (item.correctAnswer === 'c') {
-    console.log(item.correctAnswer);
-  }
-});
+quiz(questionsArray);
